e94a9edb24b3eabfa9d0545ff0c735a857eb6779
aline dias necchi ribeiro <alinenecchi@gmail.com>
2021-07-10 17:47:00 -0300

    adiciona contador de tempo na criação da arvore e no inicio do processo

--- code/main.py
+++ code/main.py
@@ -1,7 +1,9 @@
 from no import Node
 import os
+import timeit
 
 startingnodes = ""
+start_time = timeit.default_timer()
 def read_file(filename):
     letters_frequency = {}
     file = open(filename, "r")
@@ -18,6 +20,7 @@ def read_file(filename):
 
 def create_tree(x):
     print("Arquivo que está sendo comprimido: " + x)
+    start_time = timeit.default_timer()
     #global startingnodes
     letters_frequency = read_file(x)
     for i, j in letters_frequency.items():
@@ -31,6 +34,8 @@ def create_tree(x):
         del nodes[0]
         del nodes[0]
         nodes.append(n)
+    final_time = timeit.default_timer() - start_time
+    print("\nTempo de execução de criação da arvore: ",final_time)
 
 
 def print_results():
@@ -54,7 +59,6 @@ def find_top():
         top = top.parent
     return top
 
-
 def give_codes(current):
     if current.left is not None:
         current.left.code = "1"
@@ -62,6 +66,7 @@ def give_codes(current):
     if current.right is not None:
         current.right.code = "0"
         give_codes(current.right)
+    
 
 
 def creatFile(file_name, text):  # escreve um arquivo com o texto em binário
@@ -74,23 +79,26 @@ def creatFile(file_name, text):  # escreve um arquivo com o texto em binário
 
 
 startingnodes = []
-create_tree("sample.txt")
-#create_tree("input.in")
+#create_tree("sample.txt")
+create_tree("input.in")
 give_codes(find_top())
 print_results()
 
-#path_input = os.path.abspath("input.in")
-#input_size = os.path.getsize(path_input)
+path_input = os.path.abspath("input.in")
+input_size = os.path.getsize(path_input)
 
-path_sample = os.path.abspath("sample.txt")
-ascii_size = os.path.getsize(path_sample)
+#path_sample = os.path.abspath("sample.txt")
+#ascii_size = os.path.getsize(path_sample)
 
 path_encode = os.path.abspath("encode.bin")
 huff_size = os.path.getsize(path_encode)
 
-print('ASCII File Size:', ascii_size, 'bytes')
-#print('Input File Size:', input_size, 'bytes')
+#print('ASCII File Size:', ascii_size, 'bytes')
+print('Input File Size:', input_size, 'bytes')
 print('Huffman File Size:', huff_size, 'bytes')
 
-print('Redução de sample.txt {:.2f}%'.format((100-(huff_size*100/ascii_size))))
-#print('Redução de input.in {:.2f}%'.format((100-(huff_size*100/input_size))))
+#print('Redução de sample.txt {:.2f}%'.format((100-(huff_size*100/ascii_size))))
+print('Redução de input.in {:.2f}%'.format((100-(huff_size*100/input_size))))
+
+final_time = timeit.default_timer() - start_time
+print("\nTempo de execução do processo: ",final_time)
